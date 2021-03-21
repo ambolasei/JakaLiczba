2af72d233f8a06e07db08a196a3dcc80ee86cbf9
ambolasei <ambolasei@gmail.com>
2021-03-21 09:25:25 +0100

    Add project files.

---
+++ JakaLiczba/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakaLiczba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWprowadź liczbe: ");
+
+            var number = GetNumber();
+
+            if (number%2==0)
+            {
+            Console.WriteLine($"\nWprowadzona liczba {number} jest liczbą parzystą.");
+            }
+            else
+            {
+                Console.WriteLine($"\nWprowadzona liczba {number} jest liczbą nieparzystą.");
+            }
+        }
+        
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int inputedNumber))
+                {
+                Console.WriteLine("\nWprowadzono nieprawidłową liczbe. Spróbuj ponownie:");
+                continue;
+                }
+                return inputedNumber;
+            }
+
+        }
+    }
+}
